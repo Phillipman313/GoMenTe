c50349f5494437a01f7302c9673a8790394f9329
felipe <user.name>
2017-04-08 03:12:50 -0600

    Esquema de colores. Seccion de programas.

--- home/views.py
+++ home/views.py
@@ -3,6 +3,15 @@ from datetime import date
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import TipoIntegrante, Equipo
+
+def equipoPorIntegrante():
+    tipos = TipoIntegrante.objects.all()
+    integrantes = Equipo.objects.all()
+    equipo = {}
+    for tipo in tipos:
+        equipo[tipo.nombre] = integrantes.filter(idTipoIntegranteRef_id=tipo.pk)
+
 def index(request):
-    context = {'Y': date.today().year}
+    context = {'Y': date.today().year, 'datos': equipoPorIntegrante()}
     return render(request, 'home/index.html', context)
